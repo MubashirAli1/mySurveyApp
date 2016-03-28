93ba21ecbb4a5a05993027f85e5f268e2784e297
mubashir ali <mubashir.ali1@hotmail.com>
2016-03-28 22:24:26 +0500

    Some changes

--- mainpage.php
+++ mainpage.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+<link rel="stylesheet" href="mainstyle2.css">
+<script src="ajax1.js"></script>
+<script src="bootstrap1.js"></script>  
 <link rel="stylesheet" href="mystylesheet.css" type="text/css"/>
 <title>
 Survey App
